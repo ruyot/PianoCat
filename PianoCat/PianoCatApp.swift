01b4822153bb5831ff3428e9e38c2dc492a245c2
Tahmeed Tabeeb <tabeeb700@gmail.com>
2024-05-05 01:44:15 -0400

    Initial Commit

---
+++ PianoCat/PianoCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoCatApp.swift
+//  PianoCat
+//
+//  Created by Tahmeed Tabeeb on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct PianoCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
